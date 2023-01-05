a343b776bebb402a4f7507018e352d5cee1aa817
주학용 <jhyjhs5385@naver.com>
2023-01-05 17:56:31 +0900

    23년 1월 5일 파이프라인 IA + VS HLSL 다운로드

---
+++ _Shaders/Color.hlsl
@@ -0,0 +1,24 @@
+struct VertexInput
+{
+	//시멘틱스 이름 + 인덱스
+	//쉐이더에서 쓰기 편하게 값을 넘겨준다.
+	//행렬곱을 하기 때문에 곱하기 편하게 folat4로 값을 넘겨준다.
+	float4 position : POSITION0;
+	float4 color : COLOR0;
+};
+
+struct PixelInput
+{
+    float4 position : SV_Position0;
+    float4 color : COLOR0;
+};
+//진입점을 VS로 잡아서 컴파일할 것이다.
+PixelInput VS(VertexInput input)
+{
+    PixelInput output;	
+	
+    output.position = input.position;
+    output.color = input.color;
+    
+    return output;	
+}
