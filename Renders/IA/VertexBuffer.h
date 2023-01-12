0644566982ea012c232dae99b12221e7400c469b
주영태 <ilok2609@naver.com>
2023-01-12 17:55:00 +0900

    23년 1월 12일 클래스로 사용하기 위해 분리하기.

---
+++ Renders/IA/VertexBuffer.h
@@ -0,0 +1,57 @@
+#pragma once
+
+class VertexBuffer
+{
+public:
+	template <typename T>
+	void Create(const vector<T>& vertices, const D3D11_USAGE& usage = D3D11_USAGE_DEFAULT);
+
+	ComPtr<ID3D11Buffer> GetResource() const { return buffer; }
+	UINT GetStride() const { return stride; }
+	UINT GetOffset() const { return offset; }
+	UINT GetCount() const { return count; }
+
+	void SetIA();
+
+
+private:
+	ComPtr<ID3D11Buffer> buffer;
+	UINT stride = 0;
+	UINT offset = 0;
+	UINT count = 0;
+};
+
+template<typename T>
+inline void VertexBuffer::Create(const vector<T>& vertices, const D3D11_USAGE& usage)
+{
+	stride = sizeof(T);
+	count = (UINT)vertices.size();
+
+	D3D11_BUFFER_DESC desc;
+	ZeroMemory(&desc, sizeof(D3D11_BUFFER_DESC));
+	desc.Usage = usage;
+	desc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+	desc.ByteWidth = stride * count;
+
+	switch (usage)
+	{
+	case D3D11_USAGE_DEFAULT:
+		break;
+	case D3D11_USAGE_IMMUTABLE:
+		desc.CPUAccessFlags = 0;
+		break;
+	case D3D11_USAGE_DYNAMIC:
+		desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+		break;
+	case D3D11_USAGE_STAGING:
+		desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE | D3D11_CPU_ACCESS_READ;
+		break;
+	}
+
+	D3D11_SUBRESOURCE_DATA subData;
+	ZeroMemory(&subData, sizeof(D3D11_SUBRESOURCE_DATA));
+	subData.pSysMem = vertices.data();
+
+	HRESULT hr = DEVICE->CreateBuffer(&desc, &subData, &buffer);
+	CHECK(hr)
+}
