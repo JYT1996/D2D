16d7d781cb5bf9b422d55591c9bed548a06df77d
주영태 <ilok2609@naver.com>
2023-01-16 17:51:45 +0900

    23년 1월 16일 네모 출력하기 수정.

--- Program.cpp
+++ Program.cpp
@@ -5,6 +5,10 @@
 Program::Program()
 {
 	VPBuffer = make_unique<ViewProjectiondBuffer>();
+
+	SetGlobalBuffer();
+
+	rect = make_unique<ColorRect>();
 }
 
 Program::~Program()
@@ -16,14 +20,21 @@ void Program::SetGlobalBuffer()
 {
 	view = XMMatrixLookAtLH(Vector3(0, 0, 0), Vector3(0, 0, 1), Vector3(0, 1, 0));
 	projection = XMMatrixOrthographicOffCenterLH(0, WIN_DEFAULT_WIDTH, 0, WIN_DEFAULT_HEIGHT, 0, 1);
+
+	VPBuffer->SetView(view);
+	VPBuffer->SetProjection(projection);
+
 }
 
 void Program::Update()	//게임 로직의 메시지를 보내게 하는 것.
 {
 	
+
+	//rect 업데이트
 }
 
 void Program::Render()
 {
-
+	VPBuffer->SetVSBuffer(1);
+	//렌더
 }
