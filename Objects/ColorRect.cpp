966e49d0c0585b5b7c233033e575894bf25a3658
주영태 <ilok2609@naver.com>
2023-01-19 17:50:12 +0900

    23년 1월 19일 component로 기능을 분리해서 올리기.

--- Objects/ColorRect.cpp
+++ Objects/ColorRect.cpp
@@ -1,61 +1,19 @@
 #include "stdafx.h"
 #include "ColorRect.h"
 
-ColorRect::ColorRect(const Vector2& position, const Vector2& scale, const float& rotation)
-	: position(position), scale(scale), rotation(XMConvertToRadians(rotation))
+ColorRect::ColorRect(const string& name, const Vector2& position, const Vector2& scale, const float& rotation, const wstring shaderPath)
+	: Drawable(name, position, scale, rotation, shaderPath)
 {
-	vertices.assign(4, VertexColor());
-	vertices[0].position = Vector2(-0.5f, -0.5f);
-	vertices[0].color = RED;
-	vertices[1].position = Vector2(-0.5f, 0.5f);
-	vertices[1].color = RED;
-	vertices[2].position = Vector2(0.5f, -0.5f);
-	vertices[2].color = RED;
-	vertices[3].position = Vector2(0.5f, 0.5f);
-	vertices[3].color = RED;
-
-	indices = { 0, 1, 2, 2, 1, 3 };
 	
-	vertexBuffer = make_unique<VertexBuffer>();
-	vertexBuffer->Create(vertices, D3D11_USAGE_IMMUTABLE);
-	
-	vertexShader = make_unique<VertexShader>();
-	vertexShader->Create(L"_Shaders/VertexColor.hlsl", "VS");
 
-	indexBuffer = make_unique<IndexBuffer>();
-	indexBuffer->Create(indices,D3D11_USAGE_IMMUTABLE);
-	
-	inputLayout = make_unique<InputLayout>();
-	inputLayout->Create(VertexColor::descs, VertexColor::count, vertexShader->GetBlob());
-
-	pixelShader = make_unique<PixelShader>();
-	pixelShader->Create(L"_Shaders/VertexColor.hlsl", "PS");
-
-	worldBuffer = make_unique<WorldBuffer>();			
 }
 
 void ColorRect::Update()
 {
-	S = XMMatrixScalingFromVector(scale);
-	R = XMMatrixRotationZ(-rotation);
-	T = XMMatrixTranslationFromVector(position);
-	
-	world = S * R * T;	
 
-	worldBuffer->SetWorld(world);
 }
 
 void ColorRect::Render()
 {	
-	vertexBuffer->SetIA();
-	indexBuffer->SetIA();
-	inputLayout->SetIA();
-	DC->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-
-	worldBuffer->SetVSBuffer(0);
-
-	vertexShader->SetShader();
-	pixelShader->SetShader();
-
-	DC->DrawIndexed(indexBuffer->GetCount(), 0, 0);
+	
 }
--- Objects/ColorRect.h
+++ Objects/ColorRect.h
@@ -1,41 +1,14 @@
 #pragma once
 
-class ColorRect
+class ColorRect : public Drawable
 {
 public:
-	ColorRect(const Vector2& position, const Vector2& scale, const float& rotation);
+	ColorRect(const string& name, const Vector2& position, const Vector2& scale, const float& rotation, const wstring shaderPath);
 
 public:
 	void Update();
 	void Render();
 
-	Vector2 GetPosition() const { return position; }
-	Vector2 GetScale() const { return scale; }
-	float GetRotation() const { return rotation; }
-	float GetRotationDegree() const { return XMConvertToDegrees(rotation); }
-
-	void SetPosition(const Vector2& position) { this->position = position; }
-	void SetScale(const Vector2& scale) { this->scale = scale; }
-	void SetRotation(const float& rotation) { this->rotation = XMConvertToRadians(rotation); }
-
-	void Move(const Vector2& position) {  this->position += position *TIME->GetDeltaTime(); }
-	void Scale(const Vector2& scale) { this->scale += scale * TIME->GetDeltaTime(); }
-	void Rotat(const float& rotation) { this->rotation += XMConvertToRadians(rotation) * TIME->GetDeltaTime(); }
-
 private:
-	vector<VertexColor> vertices;
-	vector<UINT> indices;
-
-	unique_ptr<VertexBuffer> vertexBuffer;	
-	unique_ptr<VertexShader> vertexShader;
-	unique_ptr<IndexBuffer> indexBuffer;
-	unique_ptr<InputLayout> inputLayout;
-
-	unique_ptr<PixelShader> pixelShader;
-
-	unique_ptr<WorldBuffer> worldBuffer;
-	Matrix world, S, R, T;
-
-	Vector2 position, scale;
-	float rotation = 0.0f;
+	
 };
