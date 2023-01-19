aa61577662af6aa9507a7f08f1c0a70b1d069ccc
주영태 <ilok2609@naver.com>
2023-01-19 17:50:12 +0900

    23년 1월 19일 component로 기능을 분리해서 올리기.

---
+++ Objects/Drawable.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include "Drawable.h"
+
+Drawable::Drawable(const string& name, const Vector2& position, const Vector2& scale, const float& rotation, const wstring& shaderPath)
+	: Object(name), shaderPath(shaderPath)
+{
+	vertexBuffer = make_unique<VertexBuffer>();
+
+	vertexShader = make_unique<VertexShader>();
+	vertexShader->Create(shaderPath, "VS");
+
+	indexBuffer = make_unique<IndexBuffer>();
+
+	inputLayout = make_unique<InputLayout>();
+
+	pixelShader = make_unique<PixelShader>();
+	pixelShader->Create(shaderPath, "PS");
+	
+	AddComponent(make_shared<WorldComponent>(position, scale, rotation));
+}
+
+Drawable& Drawable::operator=(const Drawable& other)
+{
+	// TODO: 여기에 return 문을 삽입합니다.
+}
+
+void Drawable::Updata()
+{
+	SUPER::Updata();
+
+	vertexBuffer->SetIA();
+	indexBuffer->SetIA();
+	inputLayout->SetIA();
+
+	vertexShader->SetShader();
+
+	pixelShader->SetShader();
+}
+
+void Drawable::Render()
+{
+	SUPER::Render();
+}
+
+void Drawable::DrawCall(const D3D11_PRIMITIVE_TOPOLOGY& topology, const bool& indexed)
+{
+	DC->IASetPrimitiveTopology(topology);
+	
+	if (indexed)
+		DC->DrawIndexed(indexBuffer->GetCount(), 0, 0);
+	else
+		DC->Draw(vertexBuffer->GetCount(), 0);
+}
--- Program.cpp
+++ Program.cpp
@@ -7,7 +7,7 @@ Program::Program()
 	VPBuffer = make_unique<ViewProjectiondBuffer>();	
 	SetGlobalBuffer();
 
-	rect = make_unique<ColorRect>(WIN_CENTER, Vector2(100, 100), 0.0f);
+	rect = make_unique<ColorRect>(L"World", Vector2(WIN_CENTER_X, WIN_CENTER_Y),Vector2(100,100), 30.0f, "_Shaders/VertexColor.hlsl");
 }
 
 void Program::SetGlobalBuffer()
@@ -22,23 +22,23 @@ void Program::SetGlobalBuffer()
 void Program::Update()	//게임 로직의 메시지를 보내게 하는 것.
 {
 	if (INPUT->Press('W'))
-		rect->Move(Vector2(0, 300));
+		//rect->Move(Vector2(0, 300));
 	if (INPUT->Press('S'))
-		rect->Move(Vector2(0, -300));
+		//rect->Move(Vector2(0, -300));
 	if (INPUT->Press('A'))
-		rect->Move(Vector2(-300, 0));
+		//rect->Move(Vector2(-300, 0));
 	if (INPUT->Press('D'))
-		rect->Move(Vector2(300, 0));
+		//rect->Move(Vector2(300, 0));
 
 	if (INPUT->Press('Q'))
-		rect->Rotat(-100);
+		//rect->Rotat(-100);
 	if (INPUT->Press('E'))
-		rect->Rotat(100);
+		//rect->Rotat(100);
 
 	if (INPUT->Press('F'))
-		rect->Scale(Vector2(100,100));
+		//rect->Scale(Vector2(100,100));
 	if (INPUT->Press('G'))
-		rect->Scale(Vector2(-100,-100));
+		//rect->Scale(Vector2(-100,-100));
 
 	rect->Update();
 }
