557ed366bf215393b561a42564b75cd55f92880e
주영태 <hide2096@gmail.com>
2023-02-15 23:22:38 +0900

    23년 2월 15일 grayscale

--- _Shaders/Grayscale.hlsl
+++ _Shaders/Grayscale.hlsl
@@ -45,6 +45,7 @@ cbuffer SelectionBuffer : register(b0)
 float4 PS(PixelInput input) : SV_Target
 {
     float4 color = srcTex.Sample(samp, input.uv);
+    float4 color2 = 0;
     float3 grayscale = 0;
     float3 sepia = 0;
     
@@ -60,7 +61,7 @@ float4 PS(PixelInput input) : SV_Target
         //MS의 grayscale의 rgb비율
         grayscale = 0.299f * color.r + 0.587f * color.g + 0.114f * color.b;
         color = float4(grayscale, 1);
-    }    
+    }
     else if (_selection == 4)
     {
         sepia.r = dot(color.rgb, float3(0.393f, 0.769f, 0.189f));
@@ -70,8 +71,67 @@ float4 PS(PixelInput input) : SV_Target
     }
     else if (_selection == 5)
     {
+        float4 lt = srcTex.Sample(samp, 2 * input.uv);
+        float4 rt = srcTex.Sample(samp, 2 * float2(1 - input.uv.x, input.uv.y));
+        float4 lb = srcTex.Sample(samp, 2 * float2(input.uv.x, 1 - input.uv.y));
         
+        float4 rblt = srcTex.Sample(samp, 4 * float2(input.uv.x - 0.5f, input.uv.y - 0.5f));
+        float4 rbrt = srcTex.Sample(samp, 4 * float2(1 - input.uv.x, input.uv.y - 0.5f));
+        float4 rblb = srcTex.Sample(samp, 4 * float2(input.uv.x - 0.5f, 1 - input.uv.y));
+        float4 rbrb = srcTex.Sample(samp, 4 * (1 - input.uv));
+        
+        if (input.uv.x < 0.5f && input.uv.y < 0.5f)
+            color = lt;
+        if (input.uv.x > 0.5f && input.uv.y < 0.5f)
+        {
+            color = rt;
+            grayscale = 0.299f * color.r + 0.587f * color.g + 0.114f * color.b;
+            color = float4(grayscale, 1);
+        }
+        if (input.uv.x < 0.5f && input.uv.y > 0.5f)
+        {
+            color = lb;
+            sepia.r = dot(color.rgb, float3(0.393f, 0.769f, 0.189f));
+            sepia.g = dot(color.rgb, float3(0.349f, 0.686f, 0.168f));
+            sepia.b = dot(color.rgb, float3(0.272f, 0.534f, 0.131f));
+            color = float4(sepia.rgb, 1);
+        }       
+        if (input.uv.x > 0.5f && input.uv.y > 0.5f)
+        {           
+            if (input.uv.x < 0.75f && input.uv.y < 0.75f)
+            {
+                color = rblt;
+            }
+            if (input.uv.x > 0.75f && input.uv.y < 0.75f)
+            {
+                color = rbrt;
+                grayscale = 0.299f * color.r + 0.587f * color.g + 0.114f * color.b;
+                color = float4(grayscale, 1);
+            }
+            if (input.uv.x < 0.75f && input.uv.y > 0.75f)
+            {
+                color = rblb;
+                sepia.r = dot(color.rgb, float3(0.393f, 0.769f, 0.189f));
+                sepia.g = dot(color.rgb, float3(0.349f, 0.686f, 0.168f));
+                sepia.b = dot(color.rgb, float3(0.272f, 0.534f, 0.131f));
+                color = float4(sepia.rgb, 1);
+            }
+            if (input.uv.x > 0.75f && input.uv.y > 0.75f)
+            {
+                color = rbrb;
+            }
+        } 
+        
+        if (input.uv.x > 0.499f && input.uv.x < 0.501f)
+            color = float4(1, 1, 1, 1);
+        if (input.uv.y > 0.499f && input.uv.y < 0.501f)
+            color = float4(1, 1, 1, 1);
+        
+        if (input.uv.x > 0.7499f && input.uv.x < 0.751f && input.uv.y > 0.5f)
+            color = float4(1, 1, 1, 1);
+        if (input.uv.y > 0.7499f && input.uv.y < 0.752f && input.uv.x > 0.5f)
+            color = float4(1, 1, 1, 1);
     }
     
-        return color;
+    return color;
 }
