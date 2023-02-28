37e6e4a7e0a00b820f9d121095b64919f4c716ee
주영태 <hide2096@gmail.com>
2023-02-28 22:29:54 +0900

    집에서 점검

--- stdafx.h
+++ stdafx.h
@@ -78,6 +78,14 @@ using namespace SimpleMath;
 #endif
 #endif
 
+//FMOD
+#include "_Libraries/FMOD/fmod.hpp"
+#ifdef _M_X64
+#pragma comment(lib, "_Libraries/FMOD/x64/fmodL_vc.lib")
+#elif _M_IX86
+#pragma comment(lib, "_Libraries/FMOD/x86/fmodL_vc.lib")
+#endif
+
 //Meacros
 #define WIN_DEFAULT_WIDTH 1280.0f
 #define WIN_DEFAULT_HEIGHT 720.0f
